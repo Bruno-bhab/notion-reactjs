af8a358871e333cd043ba5ef47101ce61693ee60
bruno.bhab <bruno.henrique@beedoo.com.br>
2023-07-11 01:59:14 -0300

    create a initialContent based on readme

--- src/utils/initialContent.ts
+++ src/utils/initialContent.ts
@@ -1,23 +1,74 @@
 export const initialContent = `
-<p>Aqui estão algumas das principais vantagens do Tailwind CSS:</p>
+<h1>Notion-React</h1>
 
-<pre><code class="language-html">&lt;button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"&gt;Botão&lt;/button&gt;</code></pre>
+<p>Esta é uma aplicação React criada com Vite, TypeScript e tailwind. A aplicação realiza uma cópia simples do Notion, utilizando o editor de texto HTML chamado Tiptap.</p>
+
+<h2>Pré-requisitos</h2>
+
+<p>Antes de iniciar, certifique-se de ter as seguintes ferramentas instaladas em seu ambiente de desenvolvimento:</p>
 
 <ul>
-    <li><strong>Produtividade:</strong> Com o Tailwind CSS, você pode construir rapidamente interfaces consistentes e bonitas, aproveitando os utilitários pré-definidos que cobrem uma ampla gama de estilos. Isso elimina a necessidade de escrever CSS personalizado repetitivo e permite que você se concentre na lógica do seu aplicativo.</li>
-    <li><strong>Flexibilidade:</strong> Ao contrário de outras estruturas de CSS que limitam suas opções de estilo, o Tailwind CSS fornece uma ampla gama de classes utilitárias que podem ser combinadas para criar estilos personalizados. Você tem controle total sobre a aparência da sua interface, sem ser restrito por um conjunto fixo de estilos pré-definidos.</li>
-    <li><strong>Escalabilidade:</strong> O Tailwind CSS é projetado para ser altamente escalável. Seja para projetos pequenos ou grandes, ele se adapta facilmente, permitindo que você adicione e altere estilos de forma eficiente à medida que seu projeto cresce. A modularidade do Tailwind CSS torna a manutenção e o refatoramento do código uma tarefa mais fácil.</li>
-    <li><strong>Personalização:</strong> O Tailwind CSS permite que você personalize facilmente a aparência do seu projeto. Você pode estender ou substituir os estilos padrão, configurar cores personalizadas e definir suas próprias classes utilitárias. Isso dá a você total controle sobre o design final, permitindo que você crie uma identidade visual exclusiva para sua aplicação.</li>
-    <li><strong>Comunidade Ativa:</strong> O Tailwind CSS possui uma comunidade ativa e em crescimento, o que significa que há muitos recursos, documentação e exemplos disponíveis para ajudar você a aprender e dominar a estrutura. Além disso, existem vários plugins e integrações que estendem ainda mais as funcionalidades do Tailwind CSS.</li>
+    <li>Node.js</li>
+    <li>npm (gerenciador de pacotes do Node.js)</li>
 </ul>
 
-<p>Se você está procurando uma maneira eficiente e flexível de criar interfaces web bonitas, o Tailwind CSS pode ser a escolha certa. Sua abordagem única de estilos utilitários pré-definidos oferece um equilíbrio perfeito entre produtividade e personalização.</p>
+<h2>Instalação</h2>
 
-<blockquote>
-    <p>Com o Tailwind CSS, você pode estilizar seus elementos de forma rápida e fácil, aproveitando as classes utilitárias. Por exemplo:</p>
-    <pre><code class="language-html">&lt;button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"&gt;Botão&lt;/button&gt;</code></pre>
-</blockquote>
+<p>Siga os passos abaixo para configurar o ambiente e executar a aplicação:</p>
 
-<p>Experimente o Tailwind CSS e descubra como ele pode acelerar o desenvolvimento do seu próximo projeto!</p>
-<p>Leia mais sobre isso em <a href="https://tailwindcss.com/">Tailwind</a></p>
-            `
+<ol>
+    <li>Clone este repositório em seu ambiente de desenvolvimento:</li>
+    <pre><code>git clone git@github.com:Bruno-bhab/notion-reactjs.git</code></pre>
+    <li>Navegue para o diretório do projeto:</li>
+    <pre><code>cd notion-reactjs</code></pre>
+    <li>Instale as dependências do projeto usando o npm:</li>
+    <pre><code>npm install</code></pre>
+</ol>
+
+<h2>Uso</h2>
+
+<p>Após a conclusão da instalação, você pode iniciar a aplicação usando o seguinte comando:</p>
+
+<pre><code>npm run dev</code></pre>
+
+<p>Isso iniciará a aplicação em modo de desenvolvimento. Abra seu navegador e acesse <a href="http://localhost:5173">http://localhost:5173</a> para visualizar a aplicação.</p>
+
+<h2>Funcionalidades</h2>
+
+<p>A aplicação possui as seguintes funcionalidades:</p>
+
+<ul>
+    <li>Edição de texto selecionando o mesmo e escolhendo uma opção no menu suspenso.<br>
+    <img src="https://github.com/Bruno-bhab/notion-reactjs/blob/main/public/github/gif1.gif?raw=true" alt="float menu"></li>
+    <li>Formatação de linha usando o caractere '/'<br>
+    <img src="https://github.com/Bruno-bhab/notion-reactjs/blob/main/public/github/gif2.gif?raw=true" alt="side menu"></li>
+</ul>
+
+<h2>Exemplo de bloco de codigo em html</h2>
+
+<pre><code class="language-html">&lt;html&gt;
+    &lt;head&gt;
+        &lt;title&gt;Título da página&lt;/title&gt;
+    &lt;/head&gt;
+    &lt;body&gt;
+        &lt;h1&gt;Exemplo&lt;/h1&gt;
+        &lt;p&gt;Este é um parágrafo de exemplo.&lt;/p&gt;
+    &lt;/body&gt;
+&lt;/html&gt;</code></pre>
+
+<h2>Personalização</h2>
+
+<p>Você pode personalizar a aplicação de acordo com suas necessidades. O código-fonte está estruturado de forma modular e pode ser facilmente adaptado.</p>
+
+<p>O ponto de entrada principal da aplicação é o arquivo <code>src/index.tsx</code>, onde o componente raiz (<code>App</code>) é renderizado.</p>
+
+<p>Os componentes principais estão localizados no diretório <code>src/components</code>. Você pode modificar esses componentes ou adicionar novos conforme necessário.</p>
+
+<h2>Contribuição</h2>
+
+<p>Se você deseja contribuir para este projeto, sinta-se à vontade para abrir problemas (issues) ou enviar pull requests com melhorias.</p>
+
+<h2>Licença</h2>
+
+<p>Este projeto está licenciado sob a <a href="https://chat.openai.com/c/LICENSE">MIT License</a>.</p>
+`
